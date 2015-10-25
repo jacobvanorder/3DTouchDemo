1daaacc6da29aec7e4a9db6d80af141170699a2f
jake.vanorder@gmail.com <jake.vanorder@gmail.com>
2015-10-25 09:58:08 -0500

    Peek/Pop

--- ThreeDTouchDemo/ThreeDTouchDemo/ViewController.swift
+++ ThreeDTouchDemo/ThreeDTouchDemo/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIViewControllerPreviewingDelegate {
+    
+    @IBOutlet var peekButton: UIButton!
+    
+    //MARK: - A: Peek/Pop -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //MARK: A1: register as delegate
+        if traitCollection.forceTouchCapability == .Available {
+            registerForPreviewingWithDelegate(self, sourceView: peekButton)
+        }
+        else {
+            peekButton.addTarget(self, action: "pushDogViewController:", forControlEvents: .TouchUpInside)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: UIViewControllerPreviewingDelegate
+    
+    //MARK: A2 View Controller for peeking
+    internal func previewingContext(previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        //If you wanted to do this off of a Table View, you'd use:
+//        guard let indexPath = tableView.indexPathForRowAtPoint(location),
+//            cell = tableView.cellForRowAtIndexPath(indexPath) else {
+//                return nil
+//        }
+        
+        guard let dogFaceController = storyboard?.instantiateViewControllerWithIdentifier("OhHelloThereViewController") else {
+            return nil
+        }
+        dogFaceController.view.backgroundColor = .redColor()
+        return dogFaceController
     }
-
-
+    
+    //MARK: A2 View Controller for popping
+    func previewingContext(previewingContext: UIViewControllerPreviewing, commitViewController viewControllerToCommit: UIViewController) {
+        viewControllerToCommit.view.backgroundColor = .greenColor()
+        navigationController?.pushViewController(viewControllerToCommit, animated: true)
+    }
+    
+    func pushDogViewController(sender: UIButton) {
+        guard let dogFaceController = storyboard?.instantiateViewControllerWithIdentifier("OhHelloThereViewController") else {
+            return
+        }
+        self.navigationController?.pushViewController(dogFaceController, animated: true)
+    }
 }
 
